b399c4ed93a099725482abebf0b760e6fe6cac83
Anton Kuzin <kuzinanton@tut.by>
2025-12-11 11:51:29 +0300

    Update copying powershell scripts

--- Realism Overhaul/copy.ps1
+++ Realism Overhaul/copy.ps1
@@ -1,3 +1,3 @@
-$scriptsDirectory = "E:\Games\Kerbal Space Program RSS\Ships\Script"
-Copy-Item -Path "$PSScriptRoot\*" -Destination $scriptsDirectory -Include "*.ks"  -Recurse
+$targetDirectory = "E:\Games\Kerbal Space Program RSS\Ships\Script"
+Copy-Item -Path "$PSScriptRoot\*" -Destination $targetDirectory -Include "*.ks"  -Recurse
 Write-Host $(Get-Date -DisplayHint Time)
--- Stock/copy.ps1
+++ Stock/copy.ps1
@@ -1,3 +1,11 @@
-$scriptsDirectory = "E:\Games\Kerbal Space Program\Ships\Script"
-Copy-Item -Path "$PSScriptRoot\*" -Destination $scriptsDirectory -Include "*.ks"  -Recurse
+$targetDirectories = @(
+    "E:\Games\Kerbal Space Program\Ships\Script",
+    "E:\Games\Kerbal Space Program RSS\Ships\Script"
+)
+
+foreach ($dir in $targetDirectories)
+{
+    Copy-Item -Path "$PSScriptRoot\*" -Destination $dir -Include "*.ks"  -Recurse
+}
+
 Write-Host $(Get-Date -DisplayHint Time)
